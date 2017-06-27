2a134f148aab6f36bd62dc6000e6b3e1ba299900
ShadyBS <renatokrs@gmail.com>
2017-06-27 02:30:55 -0300

    Minor fix
    Minor fix

--- Simple XinZhao/Simple/Modes/Killsteal.cs
+++ Simple XinZhao/Simple/Modes/Killsteal.cs
@@ -9,13 +9,11 @@ namespace Simple.Modes
     {
         public static void DoKs()
         {
-            var q = Q.IsReady() && KillstealMenu.GetCheckbox("useQ");
-            var w = W.IsReady() && KillstealMenu.GetCheckbox("useW");
             var e = E.IsReady() && KillstealMenu.GetCheckbox("useE");
 			var r = R.IsReady() && KillstealMenu.GetCheckbox("useR");
 			foreach (var enemy in ObjectManager.Heroes.Enemies.Where(x=> x.IsValidTarget(E.Range) && !x.IsDead && !x.IsZombie))
             {
-                if (w && E.GetDamage(enemy) >= enemy.Health)
+                if (e && E.GetDamage(enemy) >= enemy.Health)
                     E.CastOnUnit(enemy);
             }
 			foreach (var enemy in ObjectManager.Heroes.Enemies.Where(x => x.IsValidTarget(R.Range) && !x.IsDead && !x.IsZombie))
